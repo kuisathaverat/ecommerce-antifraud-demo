d58743a950d81610d14b5b83370607cbc98d4952
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2022-09-26 16:19:58 +0200

    fix: refactor python tests

--- .ci/canary-tests/test_smoke.py
+++ .ci/canary-tests/test_smoke.py
@@ -8,13 +8,22 @@ from time import sleep
 def test_healthcheck():
     sleep(randint(1,2))
     url = os.getenv("SMOKE_TEST_URL")
+    req = urllib.request.Request("{}/{}".format(url, "healthcheck"))
+    with urllib.request.urlopen(req) as response:
+       the_page = response.read().decode("utf8")
+    assert the_page.find("Up") != -1
+    pass
+
+def test_greet():
+    sleep(randint(1,2))
+    url = os.getenv("SMOKE_TEST_URL")
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
        the_page = response.read().decode("utf8")
     assert the_page.find("Hello World!") != -1
     pass
 
-def test_healthcheck_with_request():
+def test_greet_with_request():
     sleep(randint(1,2))
     url = os.getenv("SMOKE_TEST_URL")
     req = urllib.request.Request("{}?name=foo".format(url))
@@ -22,13 +31,4 @@ def test_healthcheck_with_request():
        the_page = response.read().decode("utf8")
     assert the_page.find("Hello foo!") != -1
     pass
-
-# For adding slowness
-def test_multiple_healthchecks():
-    sleep(randint(1,4))
-    url = os.getenv("SMOKE_TEST_URL")
-    req = urllib.request.Request("{}?name=foo".format(url))
-    with urllib.request.urlopen(req) as response:
-       the_page = response.read().decode("utf8")
-    assert the_page.find("Hello foo!") != -1
-    pass
+   
