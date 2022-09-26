25078ed397a7c2db634b94111ecb46abbf3b7222
Ivan Fernandez Calvo <kuisathaverat@gmail.com>
2022-09-26 16:27:39 +0200

    test: configure pytest-otel

--- .ci/load-gen.groovy
+++ .ci/load-gen.groovy
@@ -9,8 +9,12 @@ pipeline {
    }
     stages {
         stage('Load gen') {
+            environment {
+                OTEL_SERVICE_NAME = "load-generator"
+            }
             steps {
                 container(name: 'ansible'){
+                    echo "${OTEL_EXPORTER_OTLP_ENDPOINT}"
                     sh(label: '', script: 'make -C .ci load-gen')
                 }
             }
